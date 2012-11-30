f993d797df02f62868a06eb306734bbb2ac80b3f
UWANTWALI ZIGAMA Didier <zigdidier@gmail.com>
2012-11-30 13:03:55 +0200

    "1000 days from old fork of rapidsms"

--- apps/ubuzima/management/commands/buildlocs.py
+++
@@ -1,4 +0,0 @@
-from ubuzima.dbmodule import *
-
-class Command(BaseCommand):
-    help = "Build our Table locations.  This command should be run daily via cron"
--- apps/ubuzima/sql/reporter.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE `reporters_reporter` ADD `telephone` VARCHAR( 13 ) NULL AFTER `role_id`;
-ALTER TABLE `reporters_reporter` ADD `cell_id` INT NULL AFTER `village`;
-ALTER TABLE `reporters_reporter` ADD `sector_id` INT NULL AFTER `cell_id`;
-ALTER TABLE `reporters_reporter` ADD `district_id` INT NULL AFTER `sector_id`;
-ALTER TABLE `reporters_reporter` ADD `province_id` INT NULL AFTER `district_id`;
-ALTER TABLE `reporters_reporter` ADD `nation_id` INT NULL AFTER `province_id`;
-
