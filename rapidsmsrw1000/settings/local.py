9357b1cb4ffe9a1776efd3f90db8a5a19d5a3429
David Ray <davidray@gmail.com>
2013-03-28 10:06:31 -0400

    adding settings directory

---
+++ rapidsmsrw1000/settings/local.py
@@ -0,0 +1,20 @@
+from .base import *
+
+#CELERY_ALWAYS_EAGER = True
+#
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'appointments',
+#        'USER': 'postgres',
+#        'PASSWORD': '',
+#        'HOST': '',
+#        'PORT': '',
+#    }
+#}
+
+
+#import warnings
+#warnings.filterwarnings(
+#        'error', r"DateTimeField received a naive datetime",
+#        RuntimeWarning, r'django\.db\.models\.fields')
